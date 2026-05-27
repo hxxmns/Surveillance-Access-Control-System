64319c31ff865338d2414d0ad2ec5fcd96e3b989
P0ruuuu <johnpaulparas818@gmail.com>
2026-05-27 23:03:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -194,9 +194,9 @@ def dashboard():
     unique_attackers = cursor.fetchone()[0]
 
     cursor.execute("""
-        SELECT device_id, event_type, status, timestamp
+        SELECT device_id, event_type, status, created_at
         FROM security_logs
-        ORDER BY timestamp DESC
+        ORDER BY created_at DESC
         LIMIT 7
     """)
     recent_alerts = cursor.fetchall()

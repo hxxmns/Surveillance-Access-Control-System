50040ee85ffef739391c1331bf6d0f715af85fca
ori-boop <alvingreg722@gmail.com>
2026-05-29 13:30:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, Response, make_response
 from database import get_connection
 from datetime import datetime, timedelta
-import time
 import uuid
 import threading
 import blocker
@@ -308,11 +307,10 @@ def generate_frames():
         with frame_lock:
             frame = latest_frame
         if frame is None:
-            time.sleep(0.05)
             continue
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        time.sleep(0.05)  # ← this is critical, must be here
+
 
 @app.route("/video_feed")
 def video_feed():

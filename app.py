a679ecedd0b57051f4b7ea1a4423469a765c6b46
P0ruuuu <johnpaulparas818@gmail.com>
2026-06-02 20:55:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -296,21 +296,26 @@ def logout():
     return redirect("/")
 
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
 @app.route("/logout-beacon", methods=["POST"])
 def logout_beacon():
-    try:
-        if "user" in session:
-            username = session["user"]
-            
-            device_id = request.cookies.get("device_id", "UNKNOWN_DEVICE")
-            
-            save_log(device_id, "Logout", "FAILED")
-            
-            session.clear()
-    except Exception as e:
-        print(f"[BEACON ERROR] Safety triggered: {e}")
+
+    if "user" in session:
+        username = session["user"]
+        device_id = get_device_id()
         
-    return "", 204
+    
+        save_log(device_id, f"Automatic Logout (Browser Closed): {username}", "FAILED")
+        
+       
+        session.clear()
+        
+    return "", 204  
 
 
 if __name__ == "__main__":
